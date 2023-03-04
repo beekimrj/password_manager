b664eace4d709d9cef3aceb29e09727ae33f2ad9
Biki Maharjan <beekimrj@gmail.com>
2023-03-04 23:09:04 +0800

    Add sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
+  get 'static_pages/home'
+  post 'sign_up', to: 'users#create'
+  get 'sign_up', to: 'users#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
