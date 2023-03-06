2c2364db654e3c1188014ad01fc11a8736755570
Biki Maharjan <beekimrj@gmail.com>
2023-03-06 18:39:24 +0800

    Use ActiveSession to record sessions.

--- app/controllers/concerns/authentication_concern.rb
+++ app/controllers/concerns/authentication_concern.rb
@@ -13,11 +13,24 @@ module AuthenticationConcern
 
   def login(user)
     reset_session
-    session[:current_user_id] = user.id
+    active_session = user.active_sessions.create!(user_agent: request.user_agent, ip_address: request.ip)
+    session[:current_active_session_id] = active_session.id
+
+    active_session
   end
 
   def logout
+    active_session = ActiveSession.find_by(id: session[:current_active_session_id])
     reset_session
+    active_session.destroy! if active_session.present?
+  end
+
+  def forget_active_session
+    cookies.delete :remember_token
+  end
+
+  def remember(active_session)
+    cookies.permanent.encrypted[:remember_token] = active_session.remember_token
   end
 
   def redirect_if_authenticated
@@ -27,7 +40,11 @@ module AuthenticationConcern
   private
 
   def current_user
-    Current.user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+    Current.user = if session[:current_active_session_id].present?
+                     ActiveSession.find_by(id: session[:current_active_session_id])&.user
+                   elsif cookies.permanent.encrypted[:remember_token].present?
+                     ActiveSession.find_by(remember_token: cookies.permanent.encrypted[:remember_token])&.user
+                   end
   end
 
   def user_signed_in?
