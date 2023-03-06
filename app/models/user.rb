aaf25cff3db217e5ce748cd071c958b4f81163cd
Biki Maharjan <beekimrj@gmail.com>
2023-03-06 18:38:27 +0800

    Add ActiveSession model to use it for security
    We're currently setting the user's ID in the session. Even though that value is encrypted, the encrypted value doesn't change since it's based on the user id which doesn't change. This means that if a bad actor were to get a copy of the session they would have access to a victim's account in perpetuity
    Ref: https://stevepolito.design/blog/rails-authentication-from-scratch/#step-18-store-session-in-the-database

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   attr_accessor :current_password
 
+  has_many :active_sessions, dependent: :destroy
+
   before_save :downcase_email
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, on: :create }, uniqueness: { case_sensitive: false }
 
